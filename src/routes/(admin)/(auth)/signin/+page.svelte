8a9a2fd2dc94216e7910ff45a4c04f26c1e82a1b
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-10-29 08:13:25 +0100

    Admin Main Pages Created

---
+++ src/routes/(admin)/(auth)/signin/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-3xl">Sign In page</h1>
+
+<style>
+</style>
---
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<section>
+	<h1 class="text-3xl">Dashboard Page</h1>
+</section>
+
+<style>
+</style>
