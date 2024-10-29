9adcfa4cd460c19b046c4bd8bbf72c93c66bf4e8
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-10-29 10:27:39 +0100

    Dashbaord side nav and alyout file created

---
+++ src/lib/components/dashbaord-sidenav.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import {
+		LayoutDashboard,
+		LogOut,
+		Newspaper,
+		NotebookPen,
+		Settings,
+		UsersRound
+	} from 'lucide-svelte';
+	import { Button } from './ui/button';
+</script>
+
+<aside>
+	<ul>
+		<li>
+			<Button
+				class="gap-2 font-openSans justify-start text-xs tracking-wide p-2"
+				variant="secondary"><LayoutDashboard size={24} strokeWidth={2} /> Dashbaord</Button
+			>
+		</li>
+
+		<li>
+			<Button
+				class="gap-2 font-openSans justify-start text-xs tracking-wide p-2"
+				variant="secondary"><NotebookPen size={24} strokeWidth={2} /> New Blog</Button
+			>
+		</li>
+
+		<li>
+			<Button
+				class="gap-2 font-openSans justify-start text-xs tracking-wide p-2"
+				variant="secondary"><Newspaper size={24} strokeWidth={2} /> Blogs</Button
+			>
+		</li>
+
+		<li>
+			<Button
+				class="gap-2 font-openSans justify-start text-xs tracking-wide p-2"
+				variant="secondary"><UsersRound size={34} strokeWidth={2} /> Subscribers</Button
+			>
+		</li>
+
+		<li>
+			<Button
+				class="gap-2 font-openSans justify-start text-xs tracking-wide p-2"
+				variant="secondary"><Settings size={24} strokeWidth={2} /> Settings</Button
+			>
+		</li>
+
+		<li>
+			<Button
+				class="gap-2 font-openSans justify-start text-xs tracking-wide p-2"
+				variant="destructive"><LogOut color="#fff" size={24} strokeWidth={2} /> Sign Out</Button
+			>
+		</li>
+	</ul>
+</aside>
+
+<style lang="postcss">
+	aside {
+		@apply w-1/5 xl:w-1/12 min-h-screen px-4 py-20;
+
+		& ul {
+			@apply flex flex-col gap-5 w-full h-full;
+
+			& li {
+				@apply w-full flex flex-col last:mt-auto;
+			}
+		}
+	}
+</style>
---
+++ src/routes/(admin)/dashboard/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import DashbaordSidenav from '$lib/components/dashbaord-sidenav.svelte';
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<section>
+	<DashbaordSidenav />
+
+	<div>
+		{@render children()}
+	</div>
+</section>
+
+<style lang="postcss">
+	section {
+		@apply w-full flex;
+
+		& div {
+			@apply flex-1;
+		}
+	}
+</style>
