99e0635852f778354963bb65217fbbc856858010
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-10-30 07:01:00 +0100

    Dashboard related pages created

---
+++ src/routes/(admin)/dashboard/settings/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<section>
+	<h1 class="text-3xl">Settings Page</h1>
+</section>
+
+<style lang="postcss">
+	section {
+		@apply outline w-full h-full;
+	}
+</style>
