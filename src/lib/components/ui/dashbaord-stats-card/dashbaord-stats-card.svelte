f2d7ee1edec1c349c7be71664529c23aeff18fd8
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-10-30 07:21:54 +0100

    Dashbaord Stats card & Header section completed

---
+++ src/lib/components/ui/dashbaord-stats-card/dashbaord-stats-card.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Card, CardContent, CardFooter, CardHeader } from '../card';
+
+	type Props = {
+		title: string;
+		value: string;
+		percentage: string;
+	};
+
+	let { title, value, percentage }: Props = $props();
+</script>
+
+<div>
+	<Card class="w-full shadow-none p-2">
+		<CardHeader><p>{title}</p></CardHeader>
+		<CardContent><h1>{value}</h1></CardContent>
+		<CardFooter><span>{percentage}% from last month</span></CardFooter>
+	</Card>
+</div>
+
+<style lang="postcss">
+	div {
+		@apply flex;
+
+		& p {
+			@apply font-openSans text-sm font-medium tracking-wider;
+		}
+
+		& h1 {
+			@apply text-lg font-bold tracking-wider;
+		}
+
+		& span {
+			@apply text-xs font-medium text-gray-500;
+		}
+	}
+</style>
--- src/routes/(admin)/dashboard/+page.svelte
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+	import DashbaordStatsCard from '$lib/components/ui/dashbaord-stats-card/dashbaord-stats-card.svelte';
 </script>
 
 <section>
-	<h1 class="text-3xl">Dashboard Page</h1>
+	<header>
+		<DashbaordStatsCard title="Total Views" value={`45,231`} percentage={`+20.1`} />
+		<DashbaordStatsCard title="Total Views" value={`45,231`} percentage={`+20.1`} />
+		<DashbaordStatsCard title="Total Views" value={`45,231`} percentage={`+20.1`} />
+		<DashbaordStatsCard title="Total Views" value={`45,231`} percentage={`+20.1`} />
+	</header>
 </section>
 
-<style>
+<style lang="postcss">
+	section {
+		@apply w-full h-full;
+
+		header {
+			@apply grid lg:grid-cols-4 gap-4;
+		}
+	}
 </style>
