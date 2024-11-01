3d870c6f290a6ddb19299bfca45aa279ba29c5c9
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-01 14:38:06 +0100

    Added back SSR for profile settings page

--- src/routes/(admin)/dashboard/settings/profile/+page.ts
+++ src/routes/(admin)/dashboard/settings/profile/+page.ts
@@ -3,8 +3,6 @@ import type { PageLoad } from './$types';
 import { profileSettingsFormSchema } from './schema';
 import { zod } from 'sveltekit-superforms/adapters';
 
-export const ssr = false;
-
 export const load: PageLoad = async () => {
 	const form = await superValidate(zod(profileSettingsFormSchema));
 
