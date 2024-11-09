5fc6a15b444d67b29905a25e6a3ceecbba3255f0
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-09 16:58:16 +0100

    Fixing Prisma/Netlify build clash bug 🐛 🐞 v3

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,5 @@
-import prisma from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 
-const db = new prisma.PrismaClient();
+const db = new PrismaClient();
 
 export default db;
