943343221ef614af57c84a7db497ee1fc71ddeb5
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-11 15:41:12 +0100

    Implement Create new Tag function

--- src/routes/(admin)/dashboard/blog-tags/+page.server.ts
+++ src/routes/(admin)/dashboard/blog-tags/+page.server.ts
@@ -2,42 +2,79 @@ import { setError, superValidate } from 'sveltekit-superforms';
 import type { Actions } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { blogTagsSchema } from './schema';
+import db from '$lib/database';
 
 export const actions = {
 	addTag: async (event) => {
 		const form = await superValidate(event, zod(blogTagsSchema));
-
-		// await new Promise((resolve) => setTimeout(() => resolve, 1000));
-
-		// WHEN THE DATABSE IS IMPLEMENTED THEN THE EXISTING TAGS WILL BE FETCHED
-		// AND THEN COMPARED WITH THE SUBMITTED FORM VALUE
-		const tag = 'Javascript';
-
-		// console.log(JSON.stringify(form, null, 2));
 
 		if (!form.valid) {
 			console.log('Failed to create Tag');
 
 			return {
 				message: 'Failed to create Tag',
+				success: false,
 				form
 			};
 		}
 
-		if (form.data.name.toLowerCase === tag.toLowerCase) {
-			setError(form, 'name', 'Tag already exists.');
+		const { name } = form.data;
+
+		try {
+			const tag = await db.tag.findUnique({ where: { name: name } });
+
+			console.log(tag);
+
+			if (tag && form.data.name.toLowerCase() === tag.name.toLowerCase()) {
+				setError(form, 'name', 'Tag already exists.');
+
+				return {
+					message: 'Tag already exists.',
+					success: false,
+					form
+				};
+			}
+
+			const adminSession = event.cookies.get('adminSession');
+
+			const author = await db.user.findUnique({
+				where: {
+					authToken: adminSession
+				}
+			});
+
+			// console.log('AUTHOR: ', author);
+
+			// console.log('EVENT AUTHOR OBJECT: ', JSON.stringify(event.locals.user, null, 2));
+
+			await db.tag.create({
+				data: {
+					name,
+					authorId: author?.id as string
+				}
+			});
 
 			return {
-				message: 'Tag already exists.',
+				message: 'Created Tag Successfully',
+				success: true,
+				form
+			};
+
+			// console.log('Created Tag Successfully');
+		} catch (error) {
+			console.log('CAUGTH TAG ERROR: ', JSON.stringify(error, null, 2));
+
+			setError(form, 'name', 'Create Tag Failed!!');
+
+			console.log('Created Tag Failed');
+
+			return {
+				message: 'Create Tag Failed!!',
+				success: false,
 				form
 			};
 		}
 
 		// console.log('Created Tag Successfully');
-
-		return {
-			message: 'Created Tag Successfully',
-			form
-		};
 	}
 } satisfies Actions;
