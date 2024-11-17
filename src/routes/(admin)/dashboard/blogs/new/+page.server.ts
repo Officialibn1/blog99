ee7612cad875edeb3e1fd1568c3f84cf57f6b1f5
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-17 02:54:22 +0100

    Integrate New Blog Pgae server action to the new blog API endpoint

--- src/routes/(admin)/dashboard/blogs/new/+page.server.ts
+++ src/routes/(admin)/dashboard/blogs/new/+page.server.ts
@@ -1,27 +1,40 @@
-import { superValidate } from 'sveltekit-superforms';
+import { fail, superValidate } from 'sveltekit-superforms';
 import type { Actions } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { createBlogFormSchema } from './schema';
 
 export const actions = {
-	default: async (event) => {
-		const form = await superValidate(event, zod(createBlogFormSchema));
-
-		// console.log(JSON.stringify(form, null, 2));
+	default: async ({ request, fetch }) => {
+		const form = await superValidate(request, zod(createBlogFormSchema));
 
 		if (!form.valid) {
 			console.log('Failed to create blog');
 
 			return {
 				message: 'Failed to create blog',
+				success: false,
 				form
 			};
 		}
 
-		// console.log('Created blog successfully');
+		const { data } = form;
+
+		const res = await fetch('/api/blogs', {
+			method: 'POST',
+			body: JSON.stringify(data)
+		});
+
+		if (!res.ok) {
+			return fail(400, {
+				message: 'Failed to create Blog',
+				success: false,
+				form
+			});
+		}
 
 		return {
 			message: 'Created blog successfully',
+			success: true,
 			form
 		};
 	}
