fb07599e036c9898f596d0ea96e4eb7165cd297e
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-18 20:14:12 +0100

    Implement Blogs data fetching from the API endpoint

--- src/routes/(admin)/dashboard/blogs/+page.ts
+++ src/routes/(admin)/dashboard/blogs/+page.ts
@@ -1,1 +1,20 @@
+import type { PageLoad } from './$types';
+import type { Blog } from '@prisma/client';
+
 export const ssr = false;
+
+export interface BlogWithComments extends Blog {
+	comments: Array<never>[];
+}
+
+export const load = (async ({ fetch }) => {
+	const blogsResponse = await fetch(`/api/blogs`);
+
+	const blogs: BlogWithComments[] = await blogsResponse.json();
+
+	console.log(blogs);
+
+	return {
+		blogs
+	};
+}) satisfies PageLoad;
