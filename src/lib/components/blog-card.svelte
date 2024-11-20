3a6018d4e3e76699544f83307d68f807d71d37ce
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-20 10:08:52 +0100

    Replaced dummy blog card data with data from api

--- src/lib/components/blog-card.svelte
+++ src/lib/components/blog-card.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
 	import { Content, Description, Footer, Header, Root, Title } from '$lib/components/ui/card';
 	import { Separator } from '$lib/components/ui/separator';
-	import * as Avatar from '$lib/components/ui/avatar';
 	import { goto } from '$app/navigation';
-	import { cn } from '$lib/utils';
+	import { cn, formatdate } from '$lib/utils';
+	import { Badge } from './ui/badge';
+	import type { BlogWithComments } from '../../routes/(blog)/blogs/+page.server';
 
 	type Props = {
-		id: number;
+		blog: BlogWithComments;
 		className?: string;
 	};
 
-	let { id, className }: Props = $props();
+	const { blog, className }: Props = $props();
+
+	const { slug, title, description, tags, createdAt } = blog;
 </script>
 
 <Root
 	class={cn('shadow-none md:hover:shadow-md duration-200 cursor-pointer', className)}
-	onclick={() => goto(`/blogs/${id}`)}
+	onclick={() => goto(`/blogs/${slug}`)}
 >
-	<Content class="p-0 ">
+	<Content class="p-0 flex flex-col h-full">
 		<Header class="">
 			<img src="/placeholder-image.avif" alt="Card_Placeholder_Image" />
 		</Header>
@@ -25,22 +28,23 @@
 		<Separator />
 
 		<div class="p-3 pb-0">
-			<Title class="leading-loose">Card Description for {id}</Title>
+			<Title class="leading-loose text-left">
+				{title}
+			</Title>
 
-			<Description>Lorem ipsum dolor, sit amet consectetur !</Description>
+			<Description>
+				{description}
+			</Description>
 		</div>
 
-		<Footer class="flex items-center  gap-3  p-3">
-			<Avatar.Root>
-				<Avatar.Image src="/placeholder-author.png " alt="@blog9ja" />
-				<Avatar.Fallback>b9ja</Avatar.Fallback>
-			</Avatar.Root>
-
-			<div class="flex flex-col">
-				<h3 class="text-sm font-medium leading-loose">Authors Name</h3>
-
-				<p class="text-xs font-medium text-gray-400">Mav 28. 2024 • 2 min read</p>
+		<Footer class="flex flex-col  gap-3 items-start mt-auto p-3">
+			<div class="flex flex-wrap gap-2">
+				{#each tags as tag (tag.id)}
+					<Badge variant="secondary" class="text-gray-700">{tag.name}</Badge>
+				{/each}
 			</div>
+
+			<p class="text-xs font-medium text-gray-400">{formatdate(createdAt)} • 2 min read</p>
 		</Footer>
 	</Content>
 </Root>
