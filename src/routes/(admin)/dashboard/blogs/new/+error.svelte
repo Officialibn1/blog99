2016a3fcc793f53cbcb3715e9b2c5cc9315d5046
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-11-29 19:18:51 +0100

    Create error file for the new blog route

---
+++ src/routes/(admin)/dashboard/blogs/new/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section>
+	<h1>
+		{$page.status}
+	</h1>
+
+	<p>
+		{$page.error?.message}
+	</p>
+</section>
+
+<style lang="postcss">
+	section {
+		@apply w-full h-full flex flex-col place-content-center gap-10;
+	}
+
+	h1 {
+		@apply text-3xl font-semibold text-red-500;
+	}
+
+	p {
+		@apply text-lg font-medium;
+	}
+</style>
