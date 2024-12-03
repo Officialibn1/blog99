3631cfb454b5e84dd12f4c37d923c5e3b77d82d4
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-12-03 12:03:00 +0100

    Adjust new blog form schema

--- src/routes/(admin)/dashboard/blogs/new/schema.ts
+++ src/routes/(admin)/dashboard/blogs/new/schema.ts
@@ -1,16 +1,16 @@
 import { z } from 'zod';
 
 export const createBlogFormSchema = z.object({
-	title: z.string().min(15),
-	description: z.string().min(15),
+	title: z.string().min(15, 'Title must be at least 15 characters'),
+	description: z.string().min(15, 'Description must be at least 15 characters'),
 	tags: z.array(z.string()).min(1, 'Please select at least one tag.'),
-	category: z.string().min(3),
+	category: z.string().min(3, 'Please select at least one category.'),
 	thumbNail: z
 		.instanceof(File)
 		.refine((file) => file.size > 0, 'Image is required')
 		.refine((file) => file.type.startsWith('image/'), 'Invalid image')
 		.refine((file) => file.size <= 5 * 1024 * 1024, 'Max Image size is 5MB'),
-	content: z.string().min(120),
+	content: z.string(),
 	published: z.boolean().default(false)
 });
 
