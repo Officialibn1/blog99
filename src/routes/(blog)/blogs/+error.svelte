b22b35a596308a677f74d561bc22702ed803c6c2
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-12-03 13:58:38 +0100

    Add error.svelte file for public blogs route

---
+++ src/routes/(blog)/blogs/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section>
+	<h1>{$page.status} ❗️ ❗️</h1>
+
+	<p>{$page.error?.message}</p>
+</section>
+
+<style>
+	section {
+		@apply w-full h-full flex flex-col gap-8 items-center justify-center;
+
+		& h1 {
+			@apply text-5xl font-semibold tracking-wider leading-loose;
+		}
+
+		& p {
+			@apply text-2xl font-medium tracking-wider;
+		}
+	}
+</style>
