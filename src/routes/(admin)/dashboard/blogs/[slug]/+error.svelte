b61ec5d193974d6cc155f773af90d853e518790b
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-12-30 04:40:53 +0100

    Feature ✨: create error page for single blog in dashboard

---
+++ src/routes/(admin)/dashboard/blogs/[slug]/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section>
+	<h1>Error Code: {$page.status}</h1>
+
+	<p>Error Message: {$page.error?.message}❗️ ❗️</p>
+</section>
+
+<style lang="postcss">
+	section {
+		@apply w-full h-full flex flex-col gap-5 items-center justify-center;
+
+		& h1 {
+			@apply text-5xl font-semibold tracking-wider leading-loose;
+		}
+
+		& p {
+			@apply text-2xl font-medium tracking-wider;
+		}
+	}
+</style>
